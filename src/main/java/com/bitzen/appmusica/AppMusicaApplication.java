e92f8415ac4530af055c804b0375b688368db8a7
Ryan <ryanfelipe.rr@gmail.com>
2024-02-15 22:29:05 -0300

    Initial commit

---
+++ src/main/java/com/bitzen/appmusica/AppMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.bitzen.appmusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitzen/appmusica/AppMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitzen.appmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
