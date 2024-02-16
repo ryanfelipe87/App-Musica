e8a81bc0a1e812f6eb6a1ec432b5b68c381ab621
Ryan <ryanfelipe.rr@gmail.com>
2024-02-15 22:53:24 -0300

    Repositories created

---
+++ src/main/java/com/bitzen/appmusica/repositories/MusicRepository.java
@@ -0,0 +1,9 @@
+package com.bitzen.appmusica.repositories;
+
+import com.bitzen.appmusica.models.Music;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicRepository extends JpaRepository<Music, Long> {
+}
