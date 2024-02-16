d603c7bb34ecd74fb639d6b4b9441bac6c0d3269
Ryan <ryanfelipe.rr@gmail.com>
2024-02-16 10:26:45 -0300

    Design Pattern Singleton

---
+++ src/main/java/com/bitzen/appmusica/patterns/LoggerSingleton.java
@@ -0,0 +1,25 @@
+package com.bitzen.appmusica.patterns;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggerSingleton {
+
+    private static Logger logger;
+
+    private LoggerSingleton() {
+        logger = Logger.getLogger("MyLogger");
+        logger.setLevel(Level.INFO);
+    }
+
+    public static Logger getLogger() {
+        if (logger == null) {
+            synchronized (LoggerSingleton.class) {
+                if(logger == null){
+                    new LoggerSingleton();
+                }
+            }
+        }
+        return logger;
+    }
+}
